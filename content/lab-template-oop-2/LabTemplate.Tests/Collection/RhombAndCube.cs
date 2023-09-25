2829efdc9393115bda659cdf956e6cf3f53f76cb
C10UD81RD <100081536+C10UD81RD@users.noreply.github.com>
2023-09-25 10:44:16 +0500

    Add files via upload

---
+++ content/lab-template-oop-2/LabTemplate.Tests/Collection/RhombAndCube.cs
@@ -0,0 +1,109 @@
+﻿// ReSharper disable All
+
+namespace LabTemplate.Tests;
+
+[TestCaseOrderer(XunitOrderer.OrdererTypeName, XunitOrderer.OrdererAssemblyName)]
+public sealed class RhombAndCube
+{
+    [InlineData(false, new double[] { double.NaN, -4, -6 })]
+    [InlineData(false, new double[] { 14.42, 4, 6 })]
+    [InlineData(true, new double[] { double.NaN, -6 })]
+    [InlineData(true, new double[] { 72, 6 })]
+    [Theory, XunitOrdererFact(1)]
+    public static void CalculatePerimeter(bool is3DFigure, double[] testData)
+    {
+        double result;
+        uint counter = 0;
+
+        try
+        {
+            result = FigureTestHelper.CalculatePerimeter(is3DFigure, testData[1..], ref counter);
+        }
+        catch (InvalidOperationException)
+        {
+            return;
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return;
+        }
+        catch (Exception error)
+        {
+            Assert.Fail($"Внимательно читаем сообщение об ошибке. Сообщение: «{error.Message}»");
+
+            return;
+        }
+
+        Assert.True(counter > 0, "Значение счётчика должно быть больше нуля.");
+
+        Assert.Equal(testData[0], result);
+    }
+
+    [InlineData(false, new double[] { double.NaN, -4, -6 })]
+    [InlineData(false, new double[] { 12, 4, 6 })]
+    [InlineData(true, new double[] { double.NaN, -6 })]
+    [InlineData(true, new double[] { 216, 6 })]
+    [Theory, XunitOrdererFact(2)]
+    public static void CalculateSquare(bool is3DFigure, double[] testData)
+    {
+        double result;
+        uint counter = 0;
+
+        try
+        {
+            result = FigureTestHelper.CalculateSquare(is3DFigure, testData[1..], ref counter);
+        }
+        catch (InvalidOperationException)
+        {
+            return;
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return;
+        }
+        catch (Exception error)
+        {
+            Assert.Fail($"Внимательно читаем сообщение об ошибке. Сообщение: «{error.Message}»");
+
+            return;
+        }
+
+        Assert.True(counter > 0, "Значение счётчика должно быть больше нуля.");
+
+        Assert.Equal(testData[0], result);
+    }
+
+    [InlineData(false, new double[] { double.NaN, -4, -6 })]
+    [InlineData(false, new double[] { double.NaN, 4, 6 })]
+    [InlineData(true, new double[] { double.NaN, -6 })]
+    [InlineData(true, new double[] { 216, 6 })]
+    [Theory, XunitOrdererFact(3)]
+    public static void CalculateVolume(bool is3DFigure, double[] testData)
+    {
+        double result;
+        uint counter = 0;
+
+        try
+        {
+            result = FigureTestHelper.CalculateVolume(is3DFigure, testData[1..], ref counter);
+        }
+        catch (InvalidOperationException)
+        {
+            return;
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return;
+        }
+        catch (Exception error)
+        {
+            Assert.Fail($"Внимательно читаем сообщение об ошибке. Сообщение: «{error.Message}»");
+
+            return;
+        }
+
+        Assert.True(counter > 0, "Значение счётчика должно быть больше нуля.");
+
+        Assert.Equal(testData[0], result);
+    }
+}
